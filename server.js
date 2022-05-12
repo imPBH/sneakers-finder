40b2cf5156f9713254d3df2a2bd3ae7619f6b38a
imPBH <alex.provo31@gmail.com>
2022-05-12 23:23:09 +0200

    added route for the homepage

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const crawler = require('./functions/crawler.js')
 
+const fs = require('fs');
 const express = require('express')
 const app = express()
 
@@ -7,12 +8,19 @@ let idShoe = 1
 let failed_wtn = []
 let failed_sx = []
 
+app.use(express.static("public"));
+app.set("views", "./views")
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    let buffer = fs.readFileSync("./sku.json")
+    let content = buffer.toString()
+    let data = JSON.parse(content)
+    res.render("index", {data: data})
+})
+
 crawler.crawl_wtn(idShoe, failed_wtn)
 	.then(() => crawler.stockx(failed_sx))
-
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
 
 app.listen(8080, () => {
     console.log("Server up and running on port 8080")
